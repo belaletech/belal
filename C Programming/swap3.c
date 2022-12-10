2436d4b5751c69e6287ab1c0d6528950bc7f1650
Belal ahmad <92047716+belaletech@users.noreply.github.com>
2022-12-10 11:20:06 +0000

    SWAP USING 3RD VARIABLE

---
+++ C Programming/swap3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter Number:\n");
+    scanf("%d",&a);
+    printf("Enter your 2nd number \n");
+    scanf("%d",&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("after swapping a=%d and b=%d",a,b);
+    return 0;
+}
