be5545697ddd41464b5aae3b33206480f369da43
Belal ahmad <92047716+belaletech@users.noreply.github.com>
2022-12-12 09:12:35 +0000

    no is positive or negative

---
+++ C Programming/no_+ve_or_ve.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int no;
+    printf("Enter any number \n");
+    scanf("%d",&no);
+    if (no>0)
+    {
+        printf("no is positive");
+         /* code */
+    }
+    if (no<0)
+    {
+       printf("no is negative"); /* code */
+    }
+    if(no==0)
+    {
+        printf("no is equal to 0");
+    }
+    
+    
+}
