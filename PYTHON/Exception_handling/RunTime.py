8e33173d868ad8f4d488f86d5232b917d6a2451c
beleletech <belalahmadswnbhr@gamil.com>
2023-03-29 12:09:36 +0530

    Exception Hadling problem

---
+++ PYTHON/Exception_handling/RunTime.py
@@ -0,0 +1,17 @@
+#i/o = first no =20,second number=0 then we will get
+#ZeroDivisionError: division by zero
+x=int(input("Enter your first number "))
+y=int(input("Enter your sencond number "))
+print("The Result:  ",x/y)
+
+
+# NameError: name 'c' is not defined
+#print(c)
+
+
+#3.Type Error: can only concatenate str (not "int") to str
+#print("10"+10)
+
+#4 ValueError: invalid literal for int() with base 10: 'hello
+#a=int(input("Enter any number"))
+#print(a)
---
+++ PYTHON/Exception_handling/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+x=int(input("Enter your first number "))
+y=int(input("Enter your sencond number "))
+print("The Result:  ",x/y)
